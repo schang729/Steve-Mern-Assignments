2042e2558d13737b39acfb74fb9c31f518a37a93
schang729 <80146189+schang729@users.noreply.github.com>
2021-07-15 16:39:45 -0700

    hw

---
+++ react/first_react_jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <div className="App"></div>
+    <h1>Hello Dojo</h1>
+    <h2>Things I need to do:</h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </>
+
+  );
+}
+
+export default App;
