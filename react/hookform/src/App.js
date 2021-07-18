ba609d63ff1f11f266322a607a177aaa72b4d7a3
schang729 <80146189+schang729@users.noreply.github.com>
2021-07-18 10:49:08 -0700

    hw

---
+++ react/hookform/src/App.js
@@ -0,0 +1,14 @@
+import react, { useState } from 'react';
+import './App.css';
+import Hookform from './components/Hookform';
+
+function App() {
+  return (
+    <div className="App">
+      <Hookform/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react/hookform/src/components/Hookform.js
@@ -0,0 +1,60 @@
+import react, { useState } from 'react';
+
+const Hookform = (props) => {
+    const [firstname, setFirstName] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+
+    const createUser = (e) => {
+        e.preventDefault();
+
+        const newUser = {firstname, lastname, email, password, confirmpassword};
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("");
+    };
+    return(
+        <>
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" value ={firstname} onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" value={lastname}onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" value ={password}onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="text" value={confirmpassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+        <p>First Name:{firstname}</p>
+        <p>Last Name:{lastname}</p>
+        <p>Email:{email}</p>
+        <p>Password:{password}</p>
+        <p>Confirm Password:{confirmpassword}</p>
+        </>
+
+        
+    );
+ 
+
+
+
+
+};
+export default Hookform;
