41138e95655255d73788c1cd156b83a6f5628032
schang729 <80146189+schang729@users.noreply.github.com>
2021-07-18 15:10:31 -0700

    hw

--- react/hookform/src/components/Hookform.js
+++ react/hookform/src/components/Hookform.js
@@ -23,46 +23,30 @@ const Hookform = (props) => {
             <div>
                 <label>First Name: </label> 
                 <input type="text" value ={firstname} onChange={ (e) => setFirstName(e.target.value) } />
-                {
-                    firstname.length > 0 && firstname.length < 3 ? <p>The first name must be at least 2 characters</p> : null
-                }
             </div>
             <div>
                 <label>Last Name: </label> 
                 <input type="text" value={lastname}onChange={ (e) => setLastName(e.target.value) } />
-                
-                {
-                    lastname.length > 0 && lastname.length < 3 ? <p>The last name must be at least 2 characters</p> : null
-                }
             </div>
             <div>
                 <label>Email Address: </label> 
                 <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
-                {
-                    email.length > 0 && email.length < 5 ? <p>The email must be at least 5 characters</p> : null
-                }
             </div>
             <div>
                 <label>Password: </label>
                 <input type="text" value ={password}onChange={ (e) => setPassword(e.target.value) } />
-                {
-                    password.length > 0 && password.length < 8 ? <p>The email must be at least 8 characters</p> : null
-                }
             </div>
             <div>
                 <label>Confirm Password: </label>
                 <input type="text" value={confirmpassword} onChange={ (e) => setConfirmPassword(e.target.value) } />
-                {
-                    confirmpassword == password ? null : <p>Password do not match!!</p>
-                }
             </div>
             <input type="submit" value="Create User" />
         </form>
-        {/* <p>First Name:{firstname}</p>
+        <p>First Name:{firstname}</p>
         <p>Last Name:{lastname}</p>
         <p>Email:{email}</p>
         <p>Password:{password}</p>
-        <p>Confirm Password:{confirmpassword}</p> */}
+        <p>Confirm Password:{confirmpassword}</p>
         </>
 
         
